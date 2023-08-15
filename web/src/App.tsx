733741a43207d182ab02e65d157c0504e249d4db
kosuke-17 <tamutamu17@icloud.com>
2023-08-15 12:40:24 +0900

    ad-grid導入

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AgGridReact } from 'ag-grid-react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='ag-theme-alpine' style={{ height: 400, width: 600 }}>
+      <AgGridReact
+        rowData={[
+          { make: 'Toyota', model: 'Celica', price: 35000 },
+          { make: 'Ford', model: 'Mondeo', price: 32000 },
+          { make: 'Porsche', model: 'Boxster', price: 72000 },
+        ]}
+        columnDefs={[{ field: 'make' }, { field: 'model' }, { field: 'price' }]}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
